41abd1afbfa2b7ee59acde8031a3164f249034af
NatalyaKylbanova <kylnava@gmail.com>
2016-06-07 20:45:30 +0300

    Update os_lr2.c

--- os_lr2.c
+++ os_lr2.c
@@ -5,75 +5,88 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main()
-{
-int i, j, ch, STDOUT_FORWARD, STDIN_FORWARD, in_word;
-char buffer[16][80];
-char* argv [16];
- i=0; j=0; in_word=0; STDOUT_FORWARD=0; STDIN_FORWARD=0;
-  printf ("$"); 
-    while ((ch=getchar())!= EOF) { 
-      
-    if (ch == ' ' || ch == '\n' || ch == '>' || ch == '<') {
+#define MAX_WORDS 16
+#define MAX_WORD_LEN 80
+
+int main() {
+ char buffer[MAX_WORDS][MAX_WORD_LEN];
+ char* argv [MAX_WORDS];
+ int i = 0; 
+ int j = 0; 
+ int in_word = 0; 
+ int STDOUT_Forward = 0; 
+ int STDIN_Forward = 0;
+ int ch;
+ printf ("$"); 
+ while ((ch=getchar())!= EOF) { 
+   if (ch == ' ' || ch == '\n' || ch == '>' || ch == '<') {
      if (in_word==1) { 
       in_word=0; 
       buffer[i][j]='\0'; 
       argv[i]=buffer[i]; 
       ++i; 
-      j=0; }
-}   else { buffer[i][j]=ch; ++j; in_word=1; };
-
-     if (ch == '>') {STDOUT_FORWARD = i;}
-     if (ch == '<') {STDIN_FORWARD = i;}
-      
-    if (ch=='\n') {
-       argv[i]=NULL;
-       pid_t pid = fork();
-       if (!pid) { // child branch
-       if (STDOUT_FORWARD!=0) {
+      j=0;
+     }
+ } else { 
+     buffer[i][j]=ch; 
+     ++j; 
+     in_word=1;
+ };
+ if (ch == '>') {
+  STDOUT_FORWARD = i;
+ }
+ if (ch == '<') {
+  STDIN_FORWARD = i;
+ }
+ if (ch=='\n') {
+    argv[i]=NULL;
+    pid_t pid = fork();
+    if (!pid) { // child branch
+      if (STDOUT_FORWARD!=0) {
        int fd = open(argv[STDOUT_FORWARD], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-               if (fd == -1) {
-               perror("open");
-               return EXIT_FAILURE;
-                             }
-
-                 // Forward STDOUT_FILENO to fd //
-               if (-1 == dup2(fd, STDOUT_FILENO)) {
-               perror("dup2");
-               return EXIT_FAILURE;
-                                                  }
-               argv[STDOUT_FORWARD]=NULL;
-}
-        if (STDIN_FORWARD!=0) {
+       if (fd == -1) {
+           perror("open");
+           return EXIT_FAILURE;
+       }
+       // Forward STDOUT_FILENO to fd //
+       if (-1 == dup2(fd, STDOUT_FILENO)) {
+           perror("dup2");
+           return EXIT_FAILURE;
+       }
+       argv[STDOUT_FORWARD]=NULL;
+      }
+      if (STDIN_FORWARD!=0) {
        int fd1 = open(argv[STDIN_FORWARD], O_RDONLY);
-               if (fd1 == -1) {
-               perror("open");
-               return EXIT_FAILURE;
-                             }
-
-                 // Forward STDIN_FILENO to fd1 //
-               if (-1 == dup2(fd1, STDIN_FILENO)) {
-               perror("dup2");
-               return EXIT_FAILURE;
-                                                  }
-               argv[STDIN_FORWARD]=NULL;
-}       
-
-        int rv = execvp(argv[0], argv);
-        if (rv == -1) {
+       if (fd1 == -1) {
+           perror("open");
+           return EXIT_FAILURE;
+       }
+       // Forward STDIN_FILENO to fd1 //
+       if (-1 == dup2(fd1, STDIN_FILENO)) {
+           perror("dup2");
+           return EXIT_FAILURE;
+       }
+       argv[STDIN_FORWARD]=NULL;
+      }       
+      int rv = execvp(argv[0], argv);
+      if (rv == -1) {
          perror("execvp");
          return EXIT_FAILURE;
-        }
-       }
-       // parent branch
-       pid = wait(NULL);
-       if (pid == -1) {
+      }
+    }
+    // parent branch
+    pid = wait(NULL);
+    if (pid == -1) {
          perror("wait");
          return EXIT_FAILURE;
-       }
-     printf ("$");
-     i=0; j=0; in_word=0; STDOUT_FORWARD=0; STDIN_FORWARD=0;
     }
-}
+    printf ("$");
+    i = 0; 
+    j = 0; 
+    in_word = 0; 
+    STDOUT_FORWARD = 0; 
+    STDIN_FORWARD = 0;
+   }
+ }
   return EXIT_SUCCESS;
 }
