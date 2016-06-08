93ba2a7b24e6c141453acd99db765b30ad429d8f
NatalyaKylbanova <kylnava@gmail.com>
2016-06-08 18:23:33 +0300

    Update os_lr1.c

--- os_lr1.c
+++ os_lr1.c
@@ -9,28 +9,28 @@
 
 int main() {
  char buffer[MAX_WORDS][MAX_WORD_LEN];
- char* argv [MAX_WORDS];
+ char* argv[MAX_WORDS];
  int i = 0; 
  int j = 0; 
  int in_word = 0;
  int ch;
- printf ("$"); 
- while ((ch=getchar()) != EOF) { 
+ printf("$"); 
+ while ((ch = getchar()) != EOF) { 
    if (ch == ' ' || ch == '\n') {
-     if (in_word==1) { 
-       in_word=0;
-       buffer[i][j]='\0'; 
-       argv[i]=buffer[i]; 
+     if (in_word == 1) { 
+       in_word = 0;
+       buffer[i][j] = '\0'; 
+       argv[i] = buffer[i]; 
        ++i; 
-       j=0; 
+       j = 0; 
      }
    } else { 
-       buffer[i][j]=ch; 
+       buffer[i][j] = ch; 
        ++j; 
-       in_word=1;
+       in_word = 1;
    }
-   if (ch=='\n') {
-     argv[i]=NULL;
+   if (ch == '\n') {
+     argv[i] = NULL;
      pid_t pid = fork();
      if (!pid) { // child branch
         int rv = execvp(argv[0], argv);
@@ -45,10 +45,10 @@ int main() {
         perror("wait");
         return EXIT_FAILURE;
      }
-     printf ("$");
-     i=0; 
-     j=0; 
-     in_word=0;
+     printf("$");
+     i = 0; 
+     j = 0; 
+     in_word = 0;
    }
  }
  return EXIT_SUCCESS;
